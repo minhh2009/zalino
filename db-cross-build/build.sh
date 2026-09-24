dea2eeab446ccdd3a83bf490ad4724022a74b6b6
minhh2009 <minhhoang29087@gmail.com>
2026-09-24 15:38:22 +0700

    fix

--- db-cross-build/build.sh
+++ db-cross-build/build.sh
@@ -6,5 +6,5 @@ ARCH="${2:-x64}"
 cd db-cross-build
 npm install node-addon-api
 
-npx node-gyp configure --target={$TARGET} --arch={$ARCH} --dist-url=https://www.electronjs.org/headers build 
+npx node-gyp configure --target=$TARGET --arch=$ARCH --dist-url=https://www.electronjs.org/headers build 
 echo "[*] Success! Binary at: build/Release/db-cross-v4-native.node"
